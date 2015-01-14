2f85e6af5e4257484b6a9ee0079d068cadf62cc3
Dom Charlesworth <dgc336@gmail.com>
2015-01-14 11:35:43 +0000

    Refactore Perl problem 1

---
+++ perl/p01.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+## Written by DGC
+## Fri,  4/05/2012
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+use warnings;
+
+## Main
+
+my $limit = 1000;
+
+print "Result -> " . (sum_divisble_by(3) + sum_divisble_by(5) - (15)) . "\n";
+
+sub sum_divisble_by {
+		my ($n) = @_;
+		my ($limit, $result, $count) = (1000, 0, 0);
+		
+		while ($count * $n < $limit) {
+				$result += $count * $n;
+				$count += 1;
+		}
+
+		return $result;
+}
